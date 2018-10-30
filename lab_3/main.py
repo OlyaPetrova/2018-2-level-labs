23f68cb94dedeface4c132db9d0aeef172458bbd
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-10-30 15:32:12 +0300

    Update main.py

--- lab_3/main.py
+++ lab_3/main.py
@@ -2,3 +2,33 @@
 Labour work #3
  Building an own N-gram model
 """
+if __name__ == '__main__':
+  with open('not_so_big_reference_text.txt', 'r') as f:
+    REFERENCE_TEXT = f.read()
+    
+def split_by_sentence(text: string) -> list:
+class WordStorage:
+  
+  def put(self, word:string) -> number:
+   
+class WordStorage:
+  
+  def get_id_of(self, word:string) -> number:
+   
+class WordStorage:
+  
+  def get_original_by(self, id:number) -> string:
+   
+   
+class WordStorage:
+  
+  def from_corpus(self, id:tuple) -> string:
+   
+def encode(storage_instance, corpus) -> list:
+   
+class NGramTrie:
+ 
+class NGramTrie:
+  
+  def fill_from_sentence(self, sentence: tuple) -> string:
+   
